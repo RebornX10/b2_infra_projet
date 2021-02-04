d74d1e1573f36c0da9d9dba66ff82e610fdc7e51
Samuel Adone <syadone@gmail.com>
2021-02-04 17:02:49 +0100

    Update server_script.sh

---
+++ vagrant_stuff/server_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Firewall : adding port 1337/tcp and udp
+systemctl start firewalld
+setenforce 0
+firewall-cmd --add-port=1337/tcp --permanent
+firewall-cmd --add-port=1337/udp --permanent
+firewall-cmd --reload
