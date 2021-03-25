b9eeaf68a2bfe3aaf2af8b03d5d4de16b45718d3
Sam <syadone@gmail.com>
2021-03-26 00:05:02 +0100

    working console gui on webpage, working Server on the VM

--- vagrant_stuff/server_script.sh
+++ vagrant_stuff/server_script.sh
@@ -4,37 +4,72 @@
 systemctl start firewalld
 setenforce 0
 firewall-cmd --add-port=1337/tcp --permanent
-firewall-cmd --add-port=22/tcp --permamanent
+firewall-cmd --add-port=22/tcp --permanent
 firewall-cmd --add-port=22/udp --permanent
 firewall-cmd --add-port=1337/udp --permanent
+firewall-cmd --add-port=8080/udp --permanent
+firewall-cmd --add-port=8080/tcp --permament
 firewall-cmd --reload
 
-# install vim
+# install vim, wget & Java
 sudo yum install vim -y
-sudo yum install wget -y 
+sudo yum install wget -y
+sudo yum install java-11-openjdk -y
 
 # downloads the server's JAR
-wget https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar
-mkdir /home/vagrant/server
-mv server.jar /home/vagrant/server
-chown server /home/vagrant/server/server.jar
-chmod +x /home/vagrant/server/server.jar
+wget https://cdn.getbukkit.org/spigot/spigot-1.16.5.jar
+mv spigot-1.16.5.jar server.jar
+chmod +x /home/vagrant/server.jar
 
 # disabling SELinux
 setenforce 0
 
 # making the launching script:
-touch /home/vagrant/start.sh
 cat > /home/vagrant/start.sh <<EOF
 #!/bin/bash
 
-java -jar /home/vagrant/server/server.jar -Xms512mb -Xmx1gb
+java -jar -Xmx1G /home/vagrant/server.jar
 EOF
-su - server
-chown -R :server /home/server/start.sh
-chmod -R g+wrx /home/server/start.ssh
+sudo chown vagrant start.sh
+chmod 701 /home/vagrant/start.sh
+
+touch /home/vagrant/eula.txt
+cat > /home/vagrant/eula.txt <<EOF
+#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula).
+#Thu Mar 25 15:15:57 UTC 2021
+eula=true
+EOF
 
 # Grafana installation
 sudo ARCH=amd64 GCLOUD_STACK_ID="184246" GCLOUD_API_KEY="eyJrIjoiZTFhMzJhZDkwYmUwZTg5YTkwNTgwMjNhNjIwNGFlZmZhNjY3MjM0YSIsIm4iOiJiZHhnYW5nLWVhc3lzdGFydC1wcm9tLXB1Ymxpc2hlciIsImlkIjo0NzYzMzd9" /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/grafana/agent/release/production/grafanacloud-install.sh)"
 
 sudo systemctl restart grafana-agent.service
+
+# remote konsole/admin setup
+wget https://github.com/mesacarlos/WebConsole/releases/download/v2.1/WebConsole-2.1.jar
+sudo mv WebConsole-2.1.jar /home/vagrant/plugins
+
+# now run the admin panel
+pub_ip=$(curl ifconfig.me)
+
+mkdir /home/vagrant/plugins/WebConsole
+cat > config.yml <<EOF
+useSSL: false
+StoreType: JKS
+KeyStore: plugins/WebConsole/keystore.jks
+StorePassword: storepassword
+KeyPassword: keypassword
+host: $(pub_ip)
+port: 8080
+language: en
+passwords:
+  admin:
+    user1: password
+  viewer: {}
+EOF
+
+echo "SETUP COMPLETE"
+echo "   "
+echo "   "
+echo "The public ip is $(pub_ip)"
+
